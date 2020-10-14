111d72d279bd42655a2581140b1c468129806295
Mariusz Milewski <mar9mil@interia.pl>
2020-10-14 15:24:56 +0200

    Added new shop

--- main.py
+++ main.py
@@ -3,9 +3,12 @@
 lists = {
     "Piekarnia": ["Chleb", "Pączek", "Bułki"],
     "Warzywniak": ["Marchew", "Seler", "Rukola"],
+    "Papiernik": ["Zeszyt A4", "Pióro", "Atrament", "Kałamarz" #Zmiany do pierwszego do commit'a na github'a
 }
 amount = 0
 for list in lists:
     print("Idę do ",list,", kupuję tu następujące rzeczy:",lists[list])
     amount = amount + len(lists[list])
     print("W sumie kupuję",amount,"produktów.")
+    
+    
