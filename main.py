8a1ac8d9be6ac2115f82fa8fde7e3ae276a4acc0
Mariusz Milewski <mar9mil@interia.pl>
2020-10-14 14:33:17 +0200

    Added print sum items

--- main.py
+++ main.py
@@ -8,3 +8,4 @@ amount = 0
 for list in lists:
     print("Idę do ",list,", kupuję tu następujące rzeczy:",lists[list])
     amount = amount + len(lists[list])
+    print("W sumie kupuję",amount,"produktów.")
